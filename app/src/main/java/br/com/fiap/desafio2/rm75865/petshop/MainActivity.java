89f8d7ec9eef6892e47c3ec2e13bdee0adbbcae8
Marina Yumi <Marinayumi17@gmail.com>
2017-03-03 08:33:55 -0300

    Inicio do pojeto

---
+++ app/src/main/java/br/com/fiap/desafio2/rm75865/petshop/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.fiap.desafio2.rm75865.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
